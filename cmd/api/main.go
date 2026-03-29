4766be54e4e69d282504c82b664ea23b2c47ad3d
vinit-jpl <vinit.jpl@gmail.com>
2026-03-29 22:40:33 +0530

    Implement initial project structure with configuration, database connection, and migration scripts

---
+++ cmd/api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"todo-api/internal/config"
+	"todo-api/internal/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+
+	/* Config load */
+	var cfg *config.Config
+	var err error
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration: ", err)
+	}
+
+	/* estd database connection */
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	defer pool.Close()
+
+	/* Router setup */
+	var router *gin.Engine
+	router = gin.Default() // or router := gin.Default()
+	router.SetTrustedProxies(nil)
+	router.GET("/", func(c *gin.Context) {
+
+		// H =>
+		// map[string]interface{} => older go way
+		// map[string]any{} => new go way
+		c.JSON(200, gin.H{
+			"message":  "Todo API is running",
+			"status":   "Success",
+			"database": "Connected",
+		})
+	})
+
+	router.Run(":" + cfg.Port)
+
+}
---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	Port        string
+}
+
+func Load() (*Config, error) {
+	var err error = godotenv.Load()
+
+	if err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	var config *Config = &Config{
+		DatabaseURL: os.Getenv("DATABASE_URL"),
+		Port:        os.Getenv("PORT"),
+	}
+
+	return config, nil
+}
